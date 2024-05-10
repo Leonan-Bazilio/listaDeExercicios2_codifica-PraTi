93ff8a40582685da9df597ed59e66a8a5e3af672
LEONAN BAZILIO <leonanbaziliodev@gmail.com>
2024-05-10 15:39:02 -0300

    exercicio 3 feito

---
+++ exercicio3.js
@@ -0,0 +1,14 @@
+/*3. Faça um algoritmo que pergunte a distância que um passageiro deseja percorrer em
+Km. Calcule o preço da passagem, cobrando R$ 0.50 por Km para viagens até 200 Km e
+R$ 0.45 para viagens mais longas. */
+
+const prompt = require("prompt-sync")()
+
+const distance = prompt("How far do you want to travel?")
+let totalPrice
+if (distance <= 200) {
+  totalPrice = distance * 0.50
+} else {
+  totalPrice = distance * 0.45
+}
+console.log(`The total price of the travel will be ${totalPrice} reais`)

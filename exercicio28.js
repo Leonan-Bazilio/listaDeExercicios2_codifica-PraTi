e0f5f029d1cda93dd862edb7bd4d5ae024247ba7
LEONAN BAZILIO <leonanbaziliodev@gmail.com>
2024-05-17 21:23:35 -0300

    exercicio 28 feito

---
+++ exercicio28.js
@@ -0,0 +1,46 @@
+/*28. Fazer um algoritmo para receber uma matriz 10 x 10 e devolver o resultado pedido no
+item:
+a) a soma dos elementos acima da diagonal principal;
+b) a soma dos elementos abaixo da diagonal principal; */
+
+function generateRandomMatrix(numRows, numColuns) {
+  const Matrix = []
+  for (let i = 0; i < numRows; i++) {
+    const row = []
+    for (let j = 0; j < numColuns; j++) {
+      row.push(Math.floor(Math.random() * 10))
+    }
+    Matrix.push(row)
+  }
+  return Matrix
+}
+const M = generateRandomMatrix(10, 10)
+let numbersAbove = ""
+let sumAbove = 0
+let numbersBelow = ""
+let sumBelow = 0
+
+
+for (let i = 0; i < M.length; i++) {
+  for (let j = 0; j < M[i].length; j++) {
+    if (j < i) {
+      numbersBelow += `${M[i][j]} `
+      sumBelow += M[i][j]
+    } else if (j > i) {
+      sumAbove += M[i][j]
+      numbersAbove += `${M[i][j]} `
+    }
+  }
+}
+
+console.log(`Matrix M:`)
+for (row of M) {
+  console.log(row.join("  "))
+}
+
+numbersAbove = numbersAbove.trim().replaceAll(" ", " + ")
+numbersBelow = numbersBelow.trim().replaceAll(" ", " + ")
+
+
+console.log(`The sum of the numbers above the main diagonal:\n${numbersAbove} \nResults in: ${sumAbove}`)
+console.log(`The sum of the numbers below the main diagonal:\n${numbersBelow} \nResults in: ${sumBelow}`)

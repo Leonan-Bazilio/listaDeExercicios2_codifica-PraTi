1e9e0e3140df2aa443ac3a605363abe01dbf5b3d
LEONAN BAZILIO <leonanbaziliodev@gmail.com>
2024-05-18 17:23:00 -0300

    exercicio 30 feito

---
+++ exercicio30.js
@@ -0,0 +1,37 @@
+/*30. Escrever um algoritmo que lê uma matriz M(5,5) e cria 2 vetores SL(5) e SC(5) que
+contenham, respectivamente, as somas das linhas e das colunas de M. Escrever a matriz
+e os vetores criados.
+ */
+
+function generateRandomMatrix(numRows, numColuns) {
+  const Matrix = []
+  for (let i = 0; i < numRows; i++) {
+    const row = []
+    for (let j = 0; j < numColuns; j++) {
+      row.push(Math.floor(Math.random() * 101) - 50)
+    }
+    Matrix.push(row)
+  }
+  return Matrix
+}
+const M = generateRandomMatrix(5, 5)
+let sumRows = []
+let sumColuns = []
+
+for (let i = 0; i < M.length; i++) {
+  let sum = 0
+  for (let j = 0; j < M[i].length; j++) {
+    sum += M[i][j]
+    if (sumColuns[j]) {
+      sumColuns[j] += M[i][j]
+    } else {
+      sumColuns[j] = M[i][j]
+    }
+  }
+  sumRows.push(sum)
+}
+
+console.log(`Matrix M:`)
+console.log(M)
+console.log(`The array wich sum of the rows is: ${sumRows}`)
+console.log(`The array wich sum of the coluns is: ${sumColuns}`)

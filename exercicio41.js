6e4ba4a70a274b54915d006101e618829b557acd
LEONAN BAZILIO <leonanbaziliodev@gmail.com>
2024-05-24 12:50:48 -0300

    exercicio 41 feito

---
+++ exercicio41.js
@@ -0,0 +1,10 @@
+/*41. Dado o objeto pessoa com propriedades nome e idade, acesse e imprima o valor de
+idade. Adicione uma nova propriedade chamada email ao objeto pessoa que já possui
+nome e idade */
+const person = {
+  name: "leonan",
+  age: 23
+}
+console.log(person)
+person.email = "leonanbaziliodev@gmail.com"
+console.log(person)
